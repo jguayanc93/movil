41c2ffe3e1933ba0fce04a1edcfacc35a9c74c23
juan carlos <jguayancastillo21@gmail.com>
2025-12-06 13:25:21 -0500

    corregido las direcciones de los scripts q producen error

--- activador.js/botones.js
+++ activador.js/botones.js
@@ -1,15 +1,30 @@
-document.getElementById("upd-tipo-despacho").addEventListener("click",()=>{
-    document.getElementById("tipo-despacho2").disabled=false;
-})
-document.getElementById("upd-transportista").addEventListener("click",()=>{
-    document.getElementById("transportista2").disabled=false;
-})
-document.getElementById("upd-atencion").addEventListener("click",()=>{
-    document.getElementById("atencion2").disabled=false;
-})
-document.getElementById("upd-direccion").addEventListener("click",()=>{
-    document.getElementById("direccion2").disabled=false;
-})
-document.getElementById("upd-vendedor-asignado").addEventListener("click",()=>{
-    document.getElementById("vendedor-asignado2").disabled=false;
-})
+
+if(document.getElementById("upd-tipo-despacho")){
+    document.getElementById("upd-tipo-despacho").addEventListener("click",()=>{
+        document.getElementById("tipo-despacho2").disabled=false;
+    })
+}
+
+if(document.getElementById("upd-transportista")){
+    document.getElementById("upd-transportista").addEventListener("click",()=>{
+        document.getElementById("transportista2").disabled=false;
+    })
+}
+
+if(document.getElementById("upd-atencion")){
+    document.getElementById("upd-atencion").addEventListener("click",()=>{
+        document.getElementById("atencion2").disabled=false;
+    })
+}
+
+if(document.getElementById("upd-direccion")){
+    document.getElementById("upd-direccion").addEventListener("click",()=>{
+        document.getElementById("direccion2").disabled=false;
+    })
+}
+
+if(document.getElementById("upd-vendedor-asignado")){
+    document.getElementById("upd-vendedor-asignado").addEventListener("click",()=>{
+        document.getElementById("vendedor-asignado2").disabled=false;
+    })
+}
--- js/factura/identificar.js
+++ js/factura/identificar.js
@@ -1,31 +1,31 @@
 
-document.getElementById("enviar").addEventListener("click",()=>{
-    let dataenviar=new Object();
-    dataenviar.opc1=document.getElementById("leer").value;
-    let fetchobj = new Object();
-    fetchobj.method="POST";
-    fetchobj.headers={"Content-Type":"application/json"};
-    fetchobj.mode="cors";
-    fetchobj.credentials="include";
-    fetchobj.body=JSON.stringify(dataenviar);
-    fetch(rutactualisar,fetchobj)
-    .then(resultado=>resultado.ok?resultado.json():resultado.text())
-    .then(resultado=>JSON.parse(resultado))///agregado para conversion
-    .then(resultado=>{
-        objfactura=[];
-          document.getElementById("tipo-despacho").value=resultado[0][1];
-          document.getElementById("transportista").textContent=resultado[0][3];
-          document.getElementById("atencion").textContent=resultado[0][4];
-          document.getElementById("direccion").textContent=resultado[0][5];
-          document.getElementById("vendedor-asignado").value=resultado[0][6];
-          document.getElementById("observacion").value=resultado[0][7];
-          document.getElementById("orden-compra").value=resultado[0][8];
-        //   document.getElementById("pista").value=resultado[0][9];
-        //   document.getElementById("doc").value=resultado[0][10];
-          objfactura=[resultado[0][9],resultado[0][10]]
-      })
-      .catch(err=>{console.log(err);})
-  })
+// document.getElementById("enviar").addEventListener("click",()=>{
+//     let dataenviar=new Object();
+//     dataenviar.opc1=document.getElementById("leer").value;
+//     let fetchobj = new Object();
+//     fetchobj.method="POST";
+//     fetchobj.headers={"Content-Type":"application/json"};
+//     fetchobj.mode="cors";
+//     fetchobj.credentials="include";
+//     fetchobj.body=JSON.stringify(dataenviar);
+//     fetch(rutactualisar,fetchobj)
+//     .then(resultado=>resultado.ok?resultado.json():resultado.text())
+//     .then(resultado=>JSON.parse(resultado))///agregado para conversion
+//     .then(resultado=>{
+//         objfactura=[];
+//           document.getElementById("tipo-despacho").value=resultado[0][1];
+//           document.getElementById("transportista").textContent=resultado[0][3];
+//           document.getElementById("atencion").textContent=resultado[0][4];
+//           document.getElementById("direccion").textContent=resultado[0][5];
+//           document.getElementById("vendedor-asignado").value=resultado[0][6];
+//           document.getElementById("observacion").value=resultado[0][7];
+//           document.getElementById("orden-compra").value=resultado[0][8];
+//         //   document.getElementById("pista").value=resultado[0][9];
+//         //   document.getElementById("doc").value=resultado[0][10];
+//           objfactura=[resultado[0][9],resultado[0][10]]
+//       })
+//       .catch(err=>{console.log(err);})
+//   })
 
   document.getElementById("enviar1").addEventListener("click",()=>{
     let dataenviar=new Object();
