bc1d865d6fd3734d1a8305212ff76dccccd7b05f
juan carlos <jguayancastillo21@gmail.com>
2026-02-10 15:20:35 -0500

    aumentado el numero de letras para buscar al cliente

--- js/cotizacion/buscar_cliente.js
+++ js/cotizacion/buscar_cliente.js
@@ -3,5 +3,9 @@ document.getElementById("busqueda").addEventListener("input",(ev)=>{
     let busqueda=ev.target.value;
     let busqueda_anterior=document.getElementById("recorrer-clientes");
     busqueda_anterior.innerHTML="";
-    busqueda=='' ? busqueda_anterior.innerHTML="" : buscar_cliente2(busqueda);////CAMBIAR ESTO a buscar_cliente luego
+    if(busqueda.length>2){buscar_cliente2(busqueda);}
+    else{
+        busqueda_anterior.innerHTML="";
+    }
+    // busqueda=='' ? busqueda_anterior.innerHTML="" : buscar_cliente2(busqueda);////CAMBIAR ESTO a buscar_cliente luego
 })
