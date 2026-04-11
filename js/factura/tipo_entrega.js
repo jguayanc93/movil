5e61206f497b13f05c1ed425218a89e0bb25ca18
juan carlos <jguayancastillo21@gmail.com>
2026-04-11 12:51:21 -0500

    agregado la funcion para esperar los id necesarios

--- js/factura/tipo_entrega.js
+++ js/factura/tipo_entrega.js
@@ -4,8 +4,16 @@ document.getElementById("tipo-despacho2").addEventListener('input',(ev)=>{
 })
 
 
-['primera-opc', 'segunda-opc', 'tercera-opc'].forEach(id => {
-    document.getElementById(id).addEventListener("click", () => {
-        preparado(document.getElementById(id).dataset.despacho, 1, objfactura[1]);
+// ['primera-opc', 'segunda-opc', 'tercera-opc'].forEach(id => {
+//     document.getElementById(id).addEventListener("click", () => {
+//         preparado(document.getElementById(id).dataset.despacho, 1, objfactura[1]);
+//     });
+// });
+
+document.addEventListener("DOMContentLoaded",()=>{
+    ['primera-opc', 'segunda-opc', 'tercera-opc'].forEach(id => {
+        document.getElementById(id).addEventListener("click", () => {
+            preparado(document.getElementById(id).dataset.despacho, 1, objfactura[1]);
+        });
     });
-});
+})
