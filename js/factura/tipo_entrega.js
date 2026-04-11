0dbb5be53bf1c224b459d66feb4bfc25dc3ad76f
juan carlos <jguayancastillo21@gmail.com>
2026-04-10 19:32:48 -0500

    corregido la funcion para recorrer el tipo de despacho

--- js/factura/tipo_entrega.js
+++ js/factura/tipo_entrega.js
@@ -2,3 +2,10 @@ document.getElementById("tipo-despacho2").addEventListener('input',(ev)=>{
   document.getElementById("recorrer").innerHTML="";
   ev.target.value=='' ? document.getElementById("recorrer").innerHTML="" : get_despacho();
 })
+
+
+['primera-opc', 'segunda-opc', 'tercera-opc'].forEach(id => {
+    document.getElementById(id).addEventListener("click", () => {
+        preparado(document.getElementById(id).dataset.despacho, 1, objfactura[1]);
+    });
+});
